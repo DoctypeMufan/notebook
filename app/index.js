23a8a26edc5810244ebdc027b553accad041a273
DoctypeMufan <1572138807@qq.com>
2017-03-22 19:31:46 +0800

    add one

---
+++ app/index.js
@@ -0,0 +1,19 @@
+//主要核心逻辑入口
+
+const fs = require('fs')
+
+class App{
+	  constructor(){
+
+	  }
+	  initServer(){
+	  	return (request,response)=>{
+	  	fs.readFile('./public/index.html','utf8',(error,data)=>{
+	  		response.end(data)
+	  	})
+          
+      }
+   }
+}
+
+module.exports = App
